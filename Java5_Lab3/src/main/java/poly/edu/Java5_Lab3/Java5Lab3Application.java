7860b0e2bac48f52a87a1d99fc7a8cfd017ad301
AnhVu-Josep <vulva0201@gmail.com>
2025-10-02 11:09:05 +0700

    PS43932 - LeVanAnhVu - Lab3

---
+++ Java5_Lab3/src/main/java/poly/edu/Java5_Lab3/Java5Lab3Application.java
@@ -0,0 +1,13 @@
+package poly.edu.Java5_Lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java5Lab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java5Lab3Application.class, args);
+	}
+
+}
---
+++ Java5_Lab3/src/test/java/poly/edu/Java5_Lab3/Java5Lab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package poly.edu.Java5_Lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java5Lab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
